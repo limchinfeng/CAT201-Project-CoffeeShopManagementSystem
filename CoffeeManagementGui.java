a0fb79f84d4270eff06d3b9be7fd924083f75d2a
supershuaifeng <limchinfeng@gmail.com>
2022-12-26 22:04:29 +0800

    done login page

--- CoffeeManagementGui.java
+++ CoffeeManagementGui.java
@@ -1,2 +1,21 @@
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
 public class CoffeeManagementGui {
+
+    public static void mainMenu(Stage primaryStage) {
+
+        BorderPane pane = new BorderPane();
+        pane.setPadding(new Insets(10));
+        pane.setStyle("-fx-background-color: #ecf4f4");
+
+        //Add the pane to the scene and add the scene to the stage
+        Scene scene = new Scene(pane, 700, 500);
+        primaryStage.setTitle("Coffee Shop Management System");
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
 }
--- LoginGui.java
+++ LoginGui.java
@@ -2,10 +2,13 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Separator;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
@@ -14,18 +17,31 @@ import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 
 public class LoginGui extends Application {
+
+    public static ArrayList<Administrator> admin = new ArrayList<Administrator>(25);
 
     public void start(Stage primaryStage) throws Exception {
         login(primaryStage);
     }
 
     public static void login(Stage primaryStage) {
+
+        // add test username and password into array list
+        if (admin.isEmpty()) {
+            admin.add(new Administrator(157968,157968));
+            admin.add(new Administrator(123,123));
+            admin.add(new Administrator(1,1));
+        }
 
         Stage stage = new Stage();
         BorderPane pane = new BorderPane();
@@ -54,7 +70,7 @@ public class LoginGui extends Application {
         DateFormat dateFormat = new SimpleDateFormat(dateFormatString);
         String currentDate = dateFormat.format(date);
 
-        //
+
         Label dateNow = new Label(currentDate, new ImageView("Images/date.png"));
         Label time = new Label(currentTime, new ImageView("Images/time.png"));
         dateNow.setFont(Font.font("Courier", FontWeight.BOLD, FontPosture.REGULAR, 18));
@@ -67,6 +83,70 @@ public class LoginGui extends Application {
 
         header.getChildren().addAll(line1, title, line2, hb, lg);
         pane.setTop(header);
+
+        GridPane prompt = new GridPane();
+        prompt.setPadding(new Insets(20));
+        prompt.setHgap(15);
+        prompt.setVgap(15);
+
+        TextField username = new TextField();
+        TextField password = new TextField();
+        Button btLogin = new Button("Login");
+        btLogin.setFont(Font.font("Courier", FontWeight.BOLD , FontPosture.REGULAR, 15));
+        btLogin.setPrefSize(100, 50);
+
+
+        prompt.add(new Label("Username: "), 0, 0);
+        prompt.add(username, 1, 0);
+        prompt.add(new Label("Password: "), 0, 1);
+        prompt.add(password, 1, 1);
+        prompt.add(btLogin, 1, 2);
+
+        prompt.setAlignment(Pos.CENTER);
+        pane.setCenter(prompt);
+
+        Button exit = new Button("Exit Program", new ImageView("Images/exit.png"));
+        pane.setBottom(exit);
+
+
+        /**Login Button Function**/
+        btLogin.setOnAction(ev->{
+
+            try{
+                int usrn = Integer.parseInt(username.getText());
+                int pswd = Integer.parseInt(password.getText());
+                boolean login = false;
+                for(int j=0; j<admin.size(); j++)
+                {
+                    if(admin.get(j).getUsername() == usrn && admin.get(j).getPassword() == pswd)
+                    {
+                        login = true;
+                        break;
+                    }
+                }
+
+                if(login)
+                {
+                    primaryStage.close();
+                    CoffeeManagementGui.mainMenu(stage);
+                }
+                else
+                {
+                    JOptionPane.showMessageDialog(null, "Wrong ");
+                }
+            } catch(Exception e) {
+                e.printStackTrace();
+                JOptionPane.showMessageDialog(null, "Username and Password should be Integer");
+            }
+        });
+
+
+        /** Exit**/
+        exit.setOnAction(e -> {
+            primaryStage.close();
+            CoffeeManagementGui.mainMenu(stage);
+        });
+
 
         //Add the pane to the scene and add the scene to the stage
         Scene scene = new Scene(pane, 700, 500);
