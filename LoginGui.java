f43f7505be09e5589fce57f7ef09618e572a3ba2
supershuaifeng <limchinfeng@gmail.com>
2022-12-26 14:00:02 +0800

    added login header

--- LoginGui.java
+++ LoginGui.java
@@ -1,9 +1,22 @@
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class LoginGui extends Application {
@@ -19,7 +32,41 @@ public class LoginGui extends Application {
         pane.setPadding(new Insets(10));
         pane.setStyle("-fx-background-color: #ecf4f4");
 
+        /**Header Section**/
+        VBox header = new VBox(5);
+        header.setPadding(new Insets(10));
+        header.setAlignment(Pos.CENTER);
 
+        HBox hb = new HBox(350);
+        Separator line1 = new Separator();
+        Separator line2 = new Separator();
+        Text title = new Text(320, 100, "*****  CAT201 Project - Coffee Shop Management System  *****");
+        title.setFont(Font.font("Courier", FontWeight.BOLD, FontPosture.REGULAR, 20));
+
+        //Get current date
+        Date date = new Date();
+        String timeFormatString = "hh:mm:ss";
+        DateFormat timeFormat = new SimpleDateFormat(timeFormatString);
+        String currentTime = timeFormat.format(date);
+
+        //Get current time
+        String dateFormatString = "MMM d yyyy";
+        DateFormat dateFormat = new SimpleDateFormat(dateFormatString);
+        String currentDate = dateFormat.format(date);
+
+        //
+        Label dateNow = new Label(currentDate, new ImageView("Images/date.png"));
+        Label time = new Label(currentTime, new ImageView("Images/time.png"));
+        dateNow.setFont(Font.font("Courier", FontWeight.BOLD, FontPosture.REGULAR, 18));
+        time.setFont(Font.font("Courier", FontWeight.BOLD, FontPosture.REGULAR, 18));
+        hb.getChildren().addAll(dateNow, time);
+        hb.setAlignment(Pos.CENTER);
+
+        Text lg = new Text("Login");
+        lg.setFont(Font.font("Courier", FontWeight.BOLD, FontPosture.REGULAR, 40));
+
+        header.getChildren().addAll(line1, title, line2, hb, lg);
+        pane.setTop(header);
 
         //Add the pane to the scene and add the scene to the stage
         Scene scene = new Scene(pane, 700, 500);
--- out/production/CAT201-Project-CoffeeShopManagementSystem/README.md
+++ out/production/CAT201-Project-CoffeeShopManagementSystem/README.md
@@ -1,1 +1,14 @@
-# CAT201-Project-CoffeeShopManagementSystem
+# CAT201-Project-Coffee Shop Management System
+
+## Step 1
+### Import lib
+#### lib > awt jar java > all jar files
+#### lib > javafx-sdk-19 > lib (whole folder)
+
+## Step 2
+### Edit Configuration
+#### add VM options, '--module-path "<javafx-sdk-19\lib path>" --add-modules javafx.controls,javafx.fxml'
+#### make LoginGui class as main class
+
+## Step 3
+###Happy Coding
