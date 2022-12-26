8a8c82c62133fab79f996b5f6f04bf6bacad5fdc
supershuaifeng <limchinfeng@gmail.com>
2022-12-26 09:43:33 +0800

    created main stage

--- LoginGui.java
+++ LoginGui.java
@@ -1,2 +1,35 @@
-public class LoginGui {
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+
+
+public class LoginGui extends Application {
+
+    public void start(Stage primaryStage) throws Exception {
+        login(primaryStage);
+    }
+
+    public static void login(Stage primaryStage) {
+
+        Stage stage = new Stage();
+        BorderPane pane = new BorderPane();
+        pane.setPadding(new Insets(10));
+        pane.setStyle("-fx-background-color: #ecf4f4");
+
+
+
+        //Add the pane to the scene and add the scene to the stage
+        Scene scene = new Scene(pane, 700, 500);
+        primaryStage.setTitle("Login System");
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
