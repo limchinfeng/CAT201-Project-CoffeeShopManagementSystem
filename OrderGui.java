787a956b3fe27abca285230f41d0d36af48a7990
weixiang1125 <weixiangggz@gmail.com>
2022-12-30 22:20:00 +0800

    Imported packages into OrderGui

--- OrderGui.java
+++ OrderGui.java
@@ -1,5 +1,21 @@
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
+import javax.swing.*;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+
 public class OrderGui {
     public static void main(ArrayList<OrderList> orderlist, ArrayList<Coffee> coffee) {
 
