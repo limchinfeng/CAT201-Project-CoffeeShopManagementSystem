4ac8ffe6bd5c783f1271dd6cc3c12812465867cc
weixiang1125 <weixiangggz@gmail.com>
2022-12-30 23:51:50 +0800

    Added Header Section, Button Section & Center Section

--- OrderGui.java
+++ OrderGui.java
@@ -17,7 +17,45 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class OrderGui {
+
+    private static int index;
     public static void main(ArrayList<OrderList> orderlist, ArrayList<Coffee> coffee) {
+
+        ArrayList<Order> order = new ArrayList<Order>(25);
+
+        Stage stage = new Stage();
+        Stage promptStage = new Stage();
+
+        BorderPane pane = new BorderPane();
+        pane.setPadding(new Insets(10));
+
+        /**Header Section**/
+        VBox header = new VBox(5);
+        header.setPadding(new Insets(10));
+        header.setAlignment(Pos.CENTER);
+        Separator line1 = new Separator();
+        Separator line2 = new Separator();
+        Text title = new Text(320, 100, "Order Section");
+        title.setFont(Font.font("Courier", FontWeight.BOLD, FontPosture.REGULAR, 25));
+
+
+        /**Button Section**/
+        HBox orderMenu = new HBox(10);
+        VBox coffeeColumn = new VBox(20);
+        VBox sizeColumn = new VBox(20);
+        VBox typeColumn = new VBox(20);
+        VBox addOnColumn = new VBox(20);
+        Button deleteOrder = new Button("Delete", new ImageView("Images/delete.png"));
+        Button showCoffee = new Button("Coffee", new ImageView("Images/coffee.png"));
+        Button orderCoffee = new Button("Order", new ImageView("Images/menu.png"));
+        Button printOrder = new Button("Current Order", new ImageView("Images/list.png"));
+        Button addOrder = new Button("Add",new ImageView("Images/add.png"));
+        Button orderPayment = new Button("Payment",new ImageView("Images/payment.png"));
+
+        /**Center Section**/
+        HBox centre = new HBox(10);
+        centre.setSpacing(50);
+        centre.setAlignment(Pos.CENTER);
 
     }
 }
