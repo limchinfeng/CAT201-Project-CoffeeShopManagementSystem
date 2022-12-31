7b87a8dcde5dd4d565a8b1f1e398b20e9ac793dc
weixiang1125 <weixiangggz@gmail.com>
2022-12-31 12:36:42 +0800

    Added Header, Buttons and Choices into OrderGui Menu

--- OrderGui.java
+++ OrderGui.java
@@ -57,5 +57,67 @@ public class OrderGui {
         centre.setSpacing(50);
         centre.setAlignment(Pos.CENTER);
 
+
+        ToggleGroup coffeeGroup = new ToggleGroup();
+        RadioButton [] coffeeButton = new RadioButton[10];
+        Text coffeeTitle = new Text(320, 100, "Coffee");
+        Text coffeePrice = new Text(320, 100, "refer to coffee button");
+        coffeeTitle.setFont(Font.font("Courier", FontWeight.BOLD, FontPosture.REGULAR, 23));
+        coffeePrice.setFont(Font.font("Courier", FontWeight.LIGHT, FontPosture.REGULAR, 10));
+        coffeeColumn.getChildren().addAll(coffeeTitle,coffeePrice);
+        for (int j = 0; j < coffee.size(); j++)
+        {
+            coffeeButton[j] = new RadioButton(coffee.get(j).getCoffee());
+            coffeeButton[j].setFont(Font.font("Courier", FontWeight.BOLD, FontPosture.REGULAR, 15));
+            coffeeColumn.getChildren().add(coffeeButton[j]);
+            coffeeButton[j].setToggleGroup(coffeeGroup);
+        }
+
+        String [] size = {"Small","Medium","Large"};
+        ToggleGroup sizeGroup = new ToggleGroup();
+        RadioButton [] sizeButton = new RadioButton[10];
+        Text sizeTitle = new Text(320, 100, "Size");
+        Text sizePrice = new Text(320, 100, "S+RM0,M+RM2,L+RM4");
+        sizeTitle.setFont(Font.font("Courier", FontWeight.BOLD, FontPosture.REGULAR, 23));
+        sizePrice.setFont(Font.font("Courier", FontWeight.LIGHT, FontPosture.REGULAR, 10));
+        sizeColumn.getChildren().addAll(sizeTitle,sizePrice);
+        for (int j = 0; j < size.length; j++)
+        {
+            sizeButton[j] = new RadioButton(size[j]);
+            sizeButton[j].setFont(Font.font("Courier", FontWeight.BOLD, FontPosture.REGULAR, 15));
+            sizeColumn.getChildren().add(sizeButton[j]);
+            sizeButton[j].setToggleGroup(sizeGroup);
+        }
+
+        String [] type = {"Hot","Cold"};
+        ToggleGroup typeGroup = new ToggleGroup();
+        RadioButton [] typeButton = new RadioButton[10];
+        Text typeTitle = new Text(320, 100, "Type");
+        Text typePrice = new Text(320, 100, "H+RM0, C+RM0");
+        typeTitle.setFont(Font.font("Courier", FontWeight.BOLD, FontPosture.REGULAR, 23));
+        typePrice.setFont(Font.font("Courier", FontWeight.LIGHT, FontPosture.REGULAR, 10));
+        typeColumn.getChildren().addAll(typeTitle,typePrice);
+        for (int j = 0; j < type.length; j++)
+        {
+            typeButton[j] = new RadioButton(type[j]);
+            typeButton[j].setFont(Font.font("Courier", FontWeight.BOLD, FontPosture.REGULAR, 15));
+            typeColumn.getChildren().add(typeButton[j]);
+            typeButton[j].setToggleGroup(typeGroup);
+        }
+
+        String [] addOn = {"Caramel","Mocha","Double Chocolate","Strawberry",
+                "Whipped Cream","Chocolate Syrup","Berries"};
+        RadioButton [] addOnButton = new RadioButton[10];
+        Text addOnTitle = new Text(320, 100, "Add On");
+        Text addOnPrice = new Text(320, 100, "+RM1 each");
+        addOnTitle.setFont(Font.font("Courier", FontWeight.BOLD, FontPosture.REGULAR, 23));
+        addOnPrice.setFont(Font.font("Courier", FontWeight.LIGHT, FontPosture.REGULAR, 10));
+        addOnColumn.getChildren().addAll(addOnTitle,addOnPrice);
+        for (int j = 0; j < addOn.length; j++)
+        {
+            addOnButton[j] = new RadioButton(addOn[j]);
+            addOnButton[j].setFont(Font.font("Courier", FontWeight.BOLD, FontPosture.REGULAR, 15));
+            addOnColumn.getChildren().add(addOnButton[j]);
+        }
     }
 }
