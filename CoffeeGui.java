4bbd357d1ef8970e53a94d682d2fa3318a4829f1
Lee Jia Qian <leejq1123@gmail.com>
2023-01-05 15:48:34 +0800

    import packages into CoffeeGui

--- CoffeeGui.java
+++ CoffeeGui.java
@@ -1,4 +1,30 @@
 import java.util.ArrayList;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
 public class CoffeeGui {
     public static void main(ArrayList<Coffee> coffee) {
 
