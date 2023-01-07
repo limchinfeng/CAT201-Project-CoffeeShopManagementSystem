d7f3ea5a722e8f01164fe8c4fffa25d85bc1c4e0
Lee Jia Qian <leejq1123@gmail.com>
2023-01-07 13:28:04 +0800

    Add header section

--- OrderListGui.java
+++ OrderListGui.java
@@ -20,6 +20,24 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class OrderListGui {
+
+    private static int index;
+    @SuppressWarnings("unchecked")
     public static void main(ArrayList<OrderList> orderlist) {
+
+        Stage stage = new Stage();
+        Stage promptStage = new Stage();
+
+        BorderPane pane = new BorderPane();
+        pane.setPadding(new Insets(10));
+
+        /**Header Section**/
+        VBox header = new VBox(5);
+        header.setPadding(new Insets(10));
+        header.setAlignment(Pos.CENTER);
+        Separator line1 = new Separator();
+        Separator line2 = new Separator();
+        Text title = new Text(320, 100, "Order List Section");
+        title.setFont(Font.font("Courier", FontWeight.BOLD, FontPosture.REGULAR, 25));
     }
 }
