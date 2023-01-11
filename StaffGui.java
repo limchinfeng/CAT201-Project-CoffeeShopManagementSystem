ed2d72eddd2e648a029ce00ff3fa8cc541995d3d
Jiaxin79 <khooxinjia@gmail.com>
2023-01-11 23:31:27 +0800

    Add the header 'Staff Section' as the title of staff stage.

--- StaffGui.java
+++ StaffGui.java
@@ -1,18 +1,52 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-import java.util.ArrayList;
 public class StaffGui {
+
     public static void main(ArrayList<Staff> staff) {
         Stage stage = new Stage();
         Stage promptStage = new Stage();
 
+
         BorderPane pane = new BorderPane();
         pane.setPadding(new Insets(10));
 
-        
+        /**Header Section**/
+        VBox header = new VBox(5);
+        header.setPadding(new Insets(10));
+        header.setAlignment(Pos.CENTER);
+        Separator line1 = new Separator();
+        Separator line2 = new Separator();
+        Text title = new Text(320, 100, "Staff Section");
+        title.setFont(Font.font("Courier", FontWeight.BOLD, FontPosture.REGULAR, 25));
+        header.getChildren().addAll(line1, title);
+
+        pane.setTop(header);
+
         pane.setStyle("-fx-background-color: #ecf4f4");
         Scene scene = new Scene(pane, 700, 500);
         stage.setTitle("Order Section");
