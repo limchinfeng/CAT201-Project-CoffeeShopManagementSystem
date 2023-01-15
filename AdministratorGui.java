e7553fea529a67bad49ce67df0a97411f862e764
supershuaifeng <limchinfeng@gmail.com>
2023-01-15 17:30:02 +0800

    add 'add new administrator' scene and action listener

--- AdministratorGui.java
+++ AdministratorGui.java
@@ -2,9 +2,12 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.Separator;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
@@ -13,6 +16,7 @@ import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import javax.swing.*;
 import java.util.ArrayList;
 
 public class AdministratorGui {
@@ -50,6 +54,48 @@ public class AdministratorGui {
         header.getChildren().addAll(line1, title, line2, AdministratorMenu);
         pane.setTop(header);
         pane.setBottom(mainMenu);
+
+
+        /**Scene to prompt the user to enter the new administrator's details**/
+        GridPane administratorPrompt = new GridPane();
+        administratorPrompt.setPadding(new Insets(20));
+        administratorPrompt.setHgap(5);
+        administratorPrompt.setVgap(5);
+
+        TextField usrn = new TextField();
+        TextField pswd = new TextField();
+        Button btAdd = new Button("Add Administrator");
+
+        administratorPrompt.add(new Text("Key in the Administrator's details"), 0, 0);
+        administratorPrompt.add(new Label("Enter username: "), 0, 1);
+        administratorPrompt.add(usrn, 1, 1);
+        administratorPrompt.add(new Label("Enter password: "), 0, 2);
+        administratorPrompt.add(pswd, 1, 2);
+        administratorPrompt.add(btAdd, 1, 3);
+
+        Scene addaAdministratorScene = new Scene(administratorPrompt);
+
+
+
+
+        /** Action listener to add new administrator**/
+        newAdministrator.setOnAction(e->{
+
+            promptStage.setScene(addaAdministratorScene);
+            promptStage.setTitle("Administrator Details");
+            promptStage.show();
+
+            btAdd.setOnAction(ev->{
+
+                int u = Integer.parseInt(usrn.getText());
+                int p = Integer.parseInt(pswd.getText());
+                administrator.add(new Administrator(u,p));
+                JOptionPane.showMessageDialog(null, "The new Administrator's record have been added to "
+                        + "the list. You can check the list by clicking the Existing Administrators List button");
+                promptStage.close();
+            });
+        });
+
 
 
         pane.setStyle("-fx-background-color: #ecf4f4");
